d227f510514e3fb011ff3a4b55d9ffce98b02e2e
eve olynyk <eolynyk@gmail.com>
2015-07-21 06:11:14 +0000

    added error checking

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -1,11 +1,41 @@
 Posts = new Mongo.Collection('posts');
+
+Posts.allow({
+  update: function(userId, post) { return ownsDocument(userId, post); },
+  remove: function(userId, post) { return ownsDocument(userId, post); },
+});
+
+Posts.deny({
+  update: function(userId, post, fieldNames) {
+    // may only edit the following two fields:
+    return (_.without(fieldNames, 'url', 'title').length > 0);
+  }
+});
+
+validatePost =  function(post) {
+  var errors = {};
+  
+  if (!post.title)
+    errors.title = "please fill in a headline";
+  
+  if (!post.url)
+    errors.url = "please fill in a URL";
+  
+  return errors;
+  
+};
+
 Meteor.methods({
   postInsert: function(postAttributes) {
-    check(this.userId(), String);
+    check(Meteor.userId(), String);
     check(postAttributes, {
       title: String,
       url: String
     });
+    
+    var errors =  validatePost(postAttributes);
+    if (errors.title || errors.url)
+      throw new Meteor.Error('invalid-post', "You must set a title and url for your post");
     
     var postWithSameLink = Posts.findOne({url: postAttributes.url});
     if (postWithSameLink) {
